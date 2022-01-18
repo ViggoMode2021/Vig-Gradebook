23894e8934a255c59cdb802ea84df3a1d3bb9863
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2022-01-18 15:05:41 -0500

    Update gradebook.py

--- gradebook.py
+++ gradebook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
 app = Flask(__name__)
