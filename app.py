cf3c00d5604ca8d48fcc7c00dba54223a6d11682
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2022-01-23 18:11:14 -0500

    Update and rename gradebook.py to app.py

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
-
+import psycopg2.extras
 app = Flask(__name__)
 
-#TO DO:
+# TO DO:
 
-#Make photos
-#Duplicate html, update, and query pages
-#CSS
-#Add teachers
-#Add administrators
-#Add back to home button
-#Add delete option
-#Table length?
-#Add advanced queries
-#Add update integer
+# Make photos
+# Duplicate html, update, and query pages
+# CSS
+# Add teachers
+# Add administrators
+# Add back to home button
+# Add delete option
+# Table length?
+# Add advanced queries
+# Add update integer
 
 hostname = 'localhost'
 database = 'Gradebook'
 username = 'postgres'
-pwd = ''
+pwd = 'Carrotcake2021'
 port_id = 5432
 conn = None
 cur = None
@@ -54,16 +54,16 @@ def regular_verbs_worksheet_period_1():
     title_two = 'regular_verbs_worksheet_period_1'
     return render_template('regular_verbs_worksheet_period_1.html', title=title_two)
 
-
+#period_1_spanish_1 enroll student
 @app.route('/update', methods=['POST'])
 def update():
     try:
         conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id)
 
         cur = conn.cursor()
 
@@ -76,9 +76,11 @@ def update():
         graduation_year = request.form.get("graduation year")
         grade = request.form.get("grade")
 
-        title_two = "Period 1 Spanish 1 enroll or check enrollment" #This title appears once the enroll button is hit.
+        # This title appears once the enroll button is hit.
+        title_two = "Period 1 Spanish 1 enroll or check enrollment"
 
-        insert_values = [(subject, teacher, first_name, last_name, graduation_year, grade)]
+        insert_values = [(subject, teacher, first_name,
+                          last_name, graduation_year, grade)]
 
         for record in insert_values:
             cur.execute(insert_script, record)
@@ -96,70 +98,72 @@ def update():
         if conn is not None:
             conn.close()
 
-    return render_template('period_1_Spanish_1_enroll.html',title =title_two)
+    return render_template('period_1_Spanish_1_enroll.html', title=title_two)
 
-@app.route('/query', methods=['POST'])
+#period_1_spanish_1 show class roster
+@app.route('/query', methods=['GET'])
 def query():
     conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
 
     c = conn.cursor()
 
     s = "SELECT * FROM period_1_spanish_1"
     c.execute(s)
     records_2 = c.fetchall()
-    #print(records)
-
-    #loop through results
-    #print_records = ''
-    #for record_2 in records_2[0:6]:
-        #print_records += str(record_2) + "\n"
 
     conn.commit()
     conn.close()
 
-    return render_template('query_page.html', records_2 = records_2)
+    return render_template('query_page.html', records_2=records_2)
 
-@app.route('/query_regular_verbs_ws_p1', methods=['POST'])
+#period_1_spanish_1_delete_function
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_student(id):
+    conn = psycopg2.connect(
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
+
+    cur = conn.cursor()
+
+    cur.execute('DELETE FROM period_1_spanish_1 WHERE id = {0}'.format(id))
+    conn.commit()
+    return redirect(url_for('main'))
+
+@app.route('/query_regular_verbs_ws_p1', methods=['GET'])
 def query_regular_verbs_ws_p1():
     conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
 
     c = conn.cursor()
 
     s = "SELECT * FROM period_1_spanish_1"
     c.execute(s)
     records_2 = c.fetchall()
-    #print(records)
 
-    #loop through results
-    #print_records = ''
-    #for record_2 in records_2[0:6]:
-        #print_records += str(record_2) + "\n"
+    return render_template('regular_verbs_worksheet_period_1.html', records_2=records_2)
 
-    #conn.commit()
-
-    #conn.close()
-
-    return render_template('regular_verbs_worksheet_period_1.html', records_2 = records_2)
-
-@app.route('/update_2', methods=['POST'])
+#period_3_spanish_1 enroll student
+@app.route('/update_2', methods=['GET'])
 def update_2():
     try:
         conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id)
 
         cur = conn.cursor()
 
@@ -172,9 +176,11 @@ def update_2():
         graduation_year_2 = request.form.get("graduation year_2")
         grade_2 = request.form.get("grade_2")
 
-        title_two = "Period 3 Spanish 2 enroll or check enrollment" #This title appears once the enroll button is hit.
+        # This title appears once the enroll button is hit.
+        title_two = "Period 3 Spanish 2 enroll or check enrollment"
 
-        insert_values_2 = [(subject_2, teacher_2, first_name_2, last_name_2, graduation_year_2, grade_2)]
+        insert_values_2 = [(subject_2, teacher_2, first_name_2,
+                            last_name_2, graduation_year_2, grade_2)]
 
         for record in insert_values_2:
             cur.execute(insert_script_2, record)
@@ -192,16 +198,17 @@ def update_2():
         if conn is not None:
             conn.close()
 
-    return render_template('period_3_Spanish_2_enroll.html', title = title_two)
+    return render_template('period_3_Spanish_2_enroll.html', title=title_two)
 
-@app.route('/query_2', methods=['POST'])
+#
+@app.route('/query_2', methods=['GET'])
 def query_2():
     conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
 
     c = conn.cursor()
 
@@ -209,17 +216,34 @@ def query_2():
     c.execute(s_2)
     records_3 = c.fetchall()
 
-    return render_template('query_page_period_3.html', records_3 = records_3)
+    return render_template('query_page_period_3.html', records_3=records_3)
 
+#period_3_spanish_2_delete_function
+@app.route('/delete_student_2/<string:id>', methods = ['POST','GET'])
+def delete_student_2(id):
+    conn = psycopg2.connect(
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
+
+    cur = conn.cursor()
+
+    cur.execute('DELETE FROM period_3_spanish_2 WHERE id = {0}'.format(id))
+    conn.commit()
+    return redirect(url_for('main'))
+
+#period_5_spanish_2 show class roster
 @app.route('/update_3', methods=['POST'])
 def update_3():
     try:
         conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id)
 
         cur = conn.cursor()
 
@@ -232,9 +256,11 @@ def update_3():
         graduation_year_3 = request.form.get("graduation year_3")
         grade_3 = request.form.get("grade_3")
 
-        title_two = "Period 5 Spanish 2 enroll or check enrollment" #This title appears once the enroll button is hit.
+        # This title appears once the enroll button is hit.
+        title_two = "Period 5 Spanish 2 enroll or check enrollment"
 
-        insert_values_3 = [(subject_3, teacher_3, first_name_3, last_name_3, graduation_year_3, grade_3)]
+        insert_values_3 = [(subject_3, teacher_3, first_name_3,
+                            last_name_3, graduation_year_3, grade_3)]
 
         for record in insert_values_3:
             cur.execute(insert_script_3, record)
@@ -252,16 +278,16 @@ def update_3():
         if conn is not None:
             conn.close()
 
-    return render_template('period_5_Spanish_2_enroll.html', title = title_two)
+    return render_template('period_5_Spanish_2_enroll.html', title=title_two)
 
-@app.route('/query_3', methods=['POST'])
+@app.route('/query_3', methods=['GET'])
 def query_3():
     conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
 
     c = conn.cursor()
 
@@ -271,20 +297,37 @@ def query_3():
 
     conn.close()
 
-    return render_template('query_page_period_5.html', records_4 = records_4)
+    return render_template('query_page_period_5.html', records_4=records_4)
+
+#period_5_spanish_2_delete_function
+@app.route('/delete_student_3/<string:id>', methods = ['POST','GET'])
+def delete_student_3(id):
+    conn = psycopg2.connect(
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
+
+    cur = conn.cursor()
+
+    cur.execute('DELETE FROM period_5_spanish_2 WHERE id = {0}'.format(id))
+    conn.commit()
+    return redirect(url_for('main'))
 
 @app.route('/update_period_3_grades', methods=['POST'])
 def update_period_3_grades():
     return render_template('update_period_3_grades.html')
 
+
 @app.route('/update_period_3_grades_button', methods=['POST'])
 def update_period_3_grades_button():
     conn = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id)
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id)
 
     cur = conn.cursor()
 
